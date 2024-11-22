addecdc3de58331548ce88266d40b37e21d54032
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-22 23:45:35 +0300

    переопределяем методы от matrix

--- lab2/MatrixDense.h
+++ lab2/MatrixDense.h
@@ -6,6 +6,16 @@
 
 class MatrixDense : public Matrix {
     // Код класса
+public:
+
+    Matrix* add(const Matrix& other) const override;
+    Matrix* subtract(const Matrix& other) const override;
+    Matrix* elementwiseMultiply(const Matrix& other) const override;
+    Matrix* multiply(const Matrix& other) const override;
+    Matrix* transpose() const override;
+
+    void importFromFile(const std::string& filename) override;
+    void exportToFile(const std::string& filename) const override;
 };
 
 #endif // MATRIXDENSE_H 

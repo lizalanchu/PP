99e291e77c5864c1916ae2e19870971cc7454bec
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-23 23:33:18 +0300

    класс блочной матрицы, наслед. класс Matrix, переопределение методов

--- lab2/MatrixBlock.h
+++ lab2/MatrixBlock.h
@@ -1,1 +1,23 @@
+#ifndef MATRIXBLOCK_H
+#define MATRIXBLOCK_H
+
+#include "Matrix.h"
+#include <vector>
+
+class MatrixBlock  : public Matrix {
+    // Код класса
+public:
+
+    Matrix* add(const Matrix& other) const override;
+    Matrix* subtract(const Matrix& other) const override;
+    Matrix* elementwiseMultiply(const Matrix& other) const override;
+    Matrix* multiply(const Matrix& other) const override;
+    Matrix* transpose() const override;
+
+    void importFromFile(const std::string& filename) override;
+    void exportToFile(const std::string& filename) const override;
+};
+
+#endif // MATRIXBLOCK_H 
+
 

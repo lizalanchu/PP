a29572f6a0075d10b9f02cfc9d8f55975dea1970
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-23 23:37:12 +0300

    класс диагональной матрицы, наслед. класс Matrix, переопределение методов

--- lab2/MatrixDiagonal.h
+++ lab2/MatrixDiagonal.h
@@ -1,1 +1,21 @@
+#ifndef MATRIXDIAGONAL_H
+#define MATRIXDIAGONAL_H
 
+#include "Matrix.h"
+#include <vector>
+
+class MatrixDiagonal : public Matrix {
+//код
+public:
+    
+    Matrix* add(const Matrix& other) const override;
+    Matrix* subtract(const Matrix& other) const override;
+    Matrix* elementwiseMultiply(const Matrix& other) const override;
+    Matrix* multiply(const Matrix& other) const override;
+    Matrix* transpose() const override;
+
+    void importFromFile(const std::string& filename) override;
+    void exportToFile(const std::string& filename) const override;
+};
+
+#endif // MATRIXDIAGONAL_H

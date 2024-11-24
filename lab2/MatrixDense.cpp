6f7a2ae81db6971b6eabc4022cddc5645a25c551
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-24 18:51:01 +0300

    метод поэлементного умножения

--- lab2/MatrixDense.cpp
+++ lab2/MatrixDense.cpp
@@ -57,7 +57,21 @@ Matrix* MatrixDense::elementwiseMultiply(const Matrix& other) const {
     if (!otherDense || rows != otherDense->rows || cols != otherDense->cols) {
         throw std::invalid_argument("Размеры матрицы не совпадают.");
     }
+    // Создаем новую матрицу для результата.
+    MatrixDense* result = new MatrixDense(rows, cols);
+    
+    // Поэлементное умножение.
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result->data[i][j] = data[i][j] * otherDense->data[i][j];
+        }
+    }
+
+    // Возвращаем указатель на новую матрицу.
+    return result;
 }
+
+
 
 Matrix* MatrixDense::multiply(const Matrix& other) const {
     // Преобразуем указатель на другую матрицу в указатель на MatrixDense.

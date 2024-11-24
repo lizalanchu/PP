4c4281044719f8ced8c8da5495e7ed131f837610
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-24 18:14:08 +0300

    конструктор + заглушки для методов

--- lab2/MatrixBlock.cpp
+++ lab2/MatrixBlock.cpp
@@ -1,1 +1,26 @@
 #include "MatrixBlock.h"
+
+MatrixBlock::MatrixBlock(int blockRows, int blockCols, int subRows, int subCols)
+    : blockRows(blockRows), blockCols(blockCols) {
+    blocks.resize(blockRows * blockCols, MatrixDense(subRows, subCols));
+}
+
+Matrix* MatrixBlock::add(const Matrix& other) const {
+    // Реализация сложения 
+}
+
+Matrix* MatrixBlock::subtract(const Matrix& other) const {
+    // Реализация вычитания 
+}
+
+Matrix* MatrixBlock::elementwiseMultiply(const Matrix& other) const {
+    // Реализация поэлементного умножения 
+}
+
+Matrix* MatrixBlock::multiply(const Matrix& other) const {
+    // Реализация матричного умножения 
+}
+
+Matrix* MatrixBlock::transpose() const {
+    // Транспонирование
+}

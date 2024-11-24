021320b4b16c3aa96044a82d4d53fa3eef54b0f8
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-24 16:17:30 +0300

    конструктор + заглушки для методов

--- lab2/MatrixDense.cpp
+++ lab2/MatrixDense.cpp
@@ -1,1 +1,24 @@
 #include "MatrixDense.h"
+
+
+MatrixDense::MatrixDense(int rows, int cols) : rows(rows), cols(cols), data(rows, std::vector<double>(cols)) {}
+
+Matrix* MatrixDense::add(const Matrix& other) const {
+    // Реализация операции сложения
+} 
+
+Matrix* MatrixDense::subtract(const Matrix& other) const {
+    // Реализация операции вычитания
+}
+
+Matrix* MatrixDense::elementwiseMultiply(const Matrix& other) const {
+    // Реализация поэлементного умножения
+}
+
+Matrix* MatrixDense::multiply(const Matrix& other) const {
+    // Реализация матричного умножения
+}
+
+Matrix* MatrixDense::transpose() const {
+    // Реализация транспонирования
+}

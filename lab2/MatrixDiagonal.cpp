0f1300672797bd259d3b59655337f2e56a2475ac
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-24 17:16:44 +0300

    конструктор + заглушки для методов

--- lab2/MatrixDiagonal.cpp
+++ lab2/MatrixDiagonal.cpp
@@ -1,1 +1,24 @@
 #include "MatrixDiagonal.h"
+
+
+MatrixDiagonal::MatrixDiagonal(int size) : size(size), data(size) {}
+
+Matrix* MatrixDiagonal::add(const Matrix& other) const {
+    // Реализация сложения 
+}
+
+Matrix* MatrixDiagonal::subtract(const Matrix& other) const {
+    // Реализация вычитания 
+}
+
+Matrix* MatrixDiagonal::elementwiseMultiply(const Matrix& other) const {
+    // Реализация поэлементного умножения 
+}
+
+Matrix* MatrixDiagonal::multiply(const Matrix& other) const {
+    // Реализация матричного умножения 
+}
+
+Matrix* MatrixDiagonal::transpose() const {
+     // Транспонирование 
+}

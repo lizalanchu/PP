a624f06cc6fdd70a74490a0159776fac3733f333
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-24 18:44:45 +0300

    проверка, что тип и размеры совпадают

--- lab2/MatrixDense.cpp
+++ lab2/MatrixDense.cpp
@@ -1,24 +1,45 @@
 #include "MatrixDense.h"
+#include <stdexcept> // Подключение библиотеки для обработки исключений
 
 
 MatrixDense::MatrixDense(int rows, int cols) : rows(rows), cols(cols), data(rows, std::vector<double>(cols)) {}
 
-Matrix* MatrixDense::add(const Matrix& other) const {
-    // Реализация операции сложения
+Matrix* MatrixDense::add(const Matrix& other) const { 
+    // Преобразуем указатель на другую матрицу в указатель на MatrixDense.
+    const MatrixDense* otherDense = dynamic_cast<const MatrixDense*>(&other);
+    // Проверяем, что размеры совпадают.
+    if (!otherDense || rows != otherDense->rows || cols != otherDense->cols) {
+        throw std::invalid_argument("Размеры матрицы не совпадают.");
+    }
 } 
 
 Matrix* MatrixDense::subtract(const Matrix& other) const {
-    // Реализация операции вычитания
+    // Преобразуем указатель на другую матрицу в указатель на MatrixDense.
+    const MatrixDense* otherDense = dynamic_cast<const MatrixDense*>(&other);
+    // Проверяем, что размеры совпадают.
+    if (!otherDense || rows != otherDense->rows || cols != otherDense->cols) {
+        throw std::invalid_argument("Размеры матрицы не совпадают.");
+    }
 }
 
 Matrix* MatrixDense::elementwiseMultiply(const Matrix& other) const {
-    // Реализация поэлементного умножения
+    // Преобразуем указатель на другую матрицу в указатель на MatrixDense.
+    const MatrixDense* otherDense = dynamic_cast<const MatrixDense*>(&other);
+    // Проверяем, что размеры совпадают.
+    if (!otherDense || rows != otherDense->rows || cols != otherDense->cols) {
+        throw std::invalid_argument("Размеры матрицы не совпадают.");
+    }
 }
 
 Matrix* MatrixDense::multiply(const Matrix& other) const {
-    // Реализация матричного умножения
+    // Преобразуем указатель на другую матрицу в указатель на MatrixDense.
+    const MatrixDense* otherDense = dynamic_cast<const MatrixDense*>(&other);
+    // Проверяем, что количество столбцов первой матрицы совпадает с количеством строк второй.
+    if (!otherDense || rows != otherDense->rows || cols != otherDense->cols) {
+        throw std::invalid_argument("Размеры матрицы не совпадают.");
+    }
 }
 
 Matrix* MatrixDense::transpose() const {
-    // Реализация транспонирования
+    
 }

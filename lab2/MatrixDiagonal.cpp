899e8fd449789c1f6ecb616166bb5db7fb639eff
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-25 14:32:34 +0300

    метод сложения

--- lab2/MatrixDiagonal.cpp
+++ lab2/MatrixDiagonal.cpp
@@ -1,11 +1,31 @@
 #include "MatrixDiagonal.h"
-
+#include <stdexcept> // Подключение библиотеки для обработки исключений
+#include <fstream> // Файловый ввод-вывод
+#include <iostream>// Консоль ввод-вывод
 
 MatrixDiagonal::MatrixDiagonal(int size) : size(size), data(size) {}
 
 Matrix* MatrixDiagonal::add(const Matrix& other) const {
-    // Реализация сложения 
-}
+    //Приведение указателя other к типу MatrixDiagonal.
+    const MatrixDiagonal* otherDiag = dynamic_cast<const MatrixDiagonal*>(&other);
+    //Проверка, что переданная матрица того же типа и размера, иначе выбрасывается исключение.
+    if (!otherDiag || size != otherDiag->size) {
+        throw std::invalid_argument("Размеры матрицы не совпадают");
+    }
+    
+    //Создание новой диагональной матрицы для хранения результата.
+    MatrixDiagonal* result = new MatrixDiagonal(size);
+
+    //Сложение соответствующих элементов диагоналей двух матриц.
+    for (int i = 0; i < size; ++i) {
+        result->data[i] = data[i] + otherDiag->data[i];
+    }
+  
+    return result;
+  
+} 
+
+
 
 Matrix* MatrixDiagonal::subtract(const Matrix& other) const {
     // Реализация вычитания 

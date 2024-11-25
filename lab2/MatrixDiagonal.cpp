68c238216e9ff9972e7747357f45e750aa6e5e69
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2024-11-25 15:27:14 +0300

    метод вычитания

--- lab2/MatrixDiagonal.cpp
+++ lab2/MatrixDiagonal.cpp
@@ -28,7 +28,22 @@ Matrix* MatrixDiagonal::add(const Matrix& other) const {
 
 
 Matrix* MatrixDiagonal::subtract(const Matrix& other) const {
-    // Реализация вычитания 
+    //Приведение указателя other к типу MatrixDiagonal.
+    const MatrixDiagonal* otherDiag = dynamic_cast<const MatrixDiagonal*>(&other);
+    //Проверка, что переданная матрица того же типа и размера, иначе выбрасывается исключение.
+    if (!otherDiag || size != otherDiag->size) {
+        throw std::invalid_argument("Размеры матрицы не совпадают");
+    }
+    
+    //Создание новой диагональной матрицы для хранения результата.
+    MatrixDiagonal* result = new MatrixDiagonal(size);
+
+    //Сложение соответствующих элементов диагоналей двух матриц.
+    for (int i = 0; i < size; ++i) {
+        result->data[i] = data[i] - otherDiag->data[i];
+    }
+  
+    return result; 
 }
 
 Matrix* MatrixDiagonal::elementwiseMultiply(const Matrix& other) const {
